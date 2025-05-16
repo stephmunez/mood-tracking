f896840819ad3cac4dcc127b8d2adf3bfb3336d5
Stephen Muñez <sjtmunez@gmail.com>
2025-05-16 17:59:09 +0800

    feat: setup colors

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">Mood Tracking App</h1>
+  <div class="bg-light-gradient h-screen">
+    <h1 class="text-3xl font-bold underline text-light-gradient">
+      Mood Tracking App
+    </h1>
   </div>
 </template>
