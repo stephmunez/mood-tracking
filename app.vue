462aac0f86f9a1946c319428ade8dd8b8a069ef5
Stephen Muñez <sjtmunez@gmail.com>
2025-05-17 17:43:00 +0800

    feat: add pages

--- app.vue
+++ app.vue
@@ -1,7 +1,3 @@
 <template>
-  <div class="bg-light-gradient h-screen">
-    <h1 class="text-3xl font-bold underline text-light-gradient">
-      Mood Tracking App
-    </h1>
-  </div>
+  <NuxtPage />
 </template>
---
+++ pages/log-in.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-light-gradient h-screen">
+    <h1 class="text-3xl font-bold underline text-light-gradient">
+      Log in Page
+    </h1>
+  </div>
+</template>
