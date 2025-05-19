95cb461a9bb109965e2b4a8c96d40cc7ecec2636
Stephen Muñez <sjtmunez@gmail.com>
2025-05-19 16:28:35 +0800

    feat: app title and metadata

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="bg-light-gradient h-screen">
-    <h1 class="text-3xl font-bold underline text-light-gradient">
+    <h1 class="text-light-gradient text-3xl font-bold underline">
       Mood Tracking App
     </h1>
   </div>
 </template>
+
+<script setup>
+useHead({
+  title: "Mood Tracking | Home",
+});
+</script>
