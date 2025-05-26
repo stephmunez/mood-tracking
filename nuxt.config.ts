cb815fd44ebcb971fcb77460330e1d9ea5c3b395
Stephen Muñez <sjtmunez@gmail.com>
2025-05-26 15:34:48 +0800

    feat: add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/image", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/image", "@nuxtjs/tailwindcss", "@pinia/nuxt"],
   app: {
     head: {
       meta: [
