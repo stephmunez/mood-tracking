3489d6b271ef4fb74fb74a9367ec9c191ac8112b
Stephen Muñez <sjtmunez@gmail.com>
2025-06-10 10:57:20 +0800

    feat: watch user value

--- plugins/authSetup.client.js
+++ plugins/authSetup.client.js
@@ -1,7 +1,32 @@
 import { useAuthStore } from "../stores/useAuthStore";
 
+const PROTECTED_ROUTES = ["/"];
+const GUEST_ONLY_ROUTES = ["/log-in", "/sign-up"];
+
 export default defineNuxtPlugin(() => {
   const authStore = useAuthStore();
+  const router = useRouter();
 
   authStore.setupAuthListener();
+
+  const redirectUser = () => {
+    const { path } = router.currentRoute.value;
+
+    if (authStore.user) {
+      if (GUEST_ONLY_ROUTES.includes(path)) {
+        router.push("/");
+      }
+    }
+
+    if (!authStore.user) {
+      if (PROTECTED_ROUTES.includes(path)) {
+        router.push("/log-in");
+      }
+    }
+  };
+
+  watch(
+    () => authStore.user,
+    () => redirectUser(),
+  );
 });
