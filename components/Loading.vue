29a25a396adf455b58f61359fda6c3034702b9c4
Stephen Muñez <sjtmunez@gmail.com>
2025-06-11 16:15:34 +0800

    feat: loading component

---
+++ components/Loading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    v-if="authStore.user || !authStore.authReady"
+    class="flex h-screen items-center justify-center"
+  >
+    <Logo />
+  </div>
+  <slot v-else></slot>
+</template>
+
+<script setup>
+import { useAuthStore } from "../stores/useAuthStore";
+
+const authStore = useAuthStore();
+</script>
